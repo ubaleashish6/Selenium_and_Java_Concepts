e1ce2416999dcd5501166e28b85fc3e976f2218b
ubaleashish6 <ubaleashish6@gmail.com>
2019-10-12 14:46:59 +0530

    added more star pattern

--- src/starPatterns/StarPattern1.java
+++ src/starPatterns/StarPattern1.java
@@ -70,7 +70,30 @@ public class StarPattern1 {
 		}
 		System.out.println();
 	}
+	
+	/*  * * * * *
+          * * * *
+          	* * * 
+          	  * * 
+                *   */
+	System.out.println();
+	System.out.println();
+	System.out.println();
+	for(int i=5;i>=1;i--){
+		
+		for(int k=5;k>i;k--){
+			System.out.print("  ");
+		}
+		for(int j=1;j<=i;j++){
+			System.out.print("* ");
+		}
+		
+		System.out.println();
+	}
 
 	}
+	
+	
+	
 
 }
