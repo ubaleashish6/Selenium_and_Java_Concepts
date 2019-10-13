faae1e773c61c7c617dbf174e98d602dfb0f9208
ubaleashish6 <ubaleashish6@gmail.com>
2019-10-13 11:55:01 +0530

    added program for unary operator

---
+++ src/differentOperators/UnaryOperators.java
@@ -0,0 +1,20 @@
+package differentOperators;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=10;
+		System.out.println(x++);
+		System.out.println(x);
+		System.out.println(++x);
+		System.out.println(x);
+		System.out.println(x--);
+		System.out.println(x);
+		System.out.println(--x);
+		System.out.println(x);
+
+	}
+
+}
